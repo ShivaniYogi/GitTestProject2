33406f4b5a5da7f78b1972855cbbc10c551eb645
Shivani Yogi <shivaniyogi01@gmail.com>
2021-07-16 18:59:24 +0530

    First Commit

---
+++ GitTestProject2/src/com/neosoft/githubtest/GitTest.java
@@ -0,0 +1,10 @@
+package com.neosoft.githubtest;
+
+public class GitTest {
+ String code = "SE";
+ if(code == "SE")
+	 System.out.println("Software Engineer");
+ else
+	 System.out.println(" Not a Software Engineer");
+}
+}
